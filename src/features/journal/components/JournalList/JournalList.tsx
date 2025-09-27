971cbe88bd400b09a950fea4d2ce055030942ab6
KoMRaD124 <40596149+KoMRaD124@users.noreply.github.com>
2025-09-27 08:51:08 +0300

    changes

--- src/features/journal/components/JournalList/JournalList.tsx
+++ src/features/journal/components/JournalList/JournalList.tsx
@@ -3,46 +3,26 @@ import { ObjectDTO } from "src/features/journal/types/Object.ts";
 import { SortOption } from "src/features/users";
 import styles from "./JournalList.module.scss";
 import JournalItemCard from "src/features/journal/components/JournalItemCard/JournalItemCard.tsx";
+import { IconError } from "src/ui/assets/icons";
 
 interface JournalListProps {
     journalList: ObjectDTO[];
     sort: SortOption;
 }
 
-function pluralizeObjects(count: number): string {
-    const absCount = Math.abs(count) % 100;
-    const lastDigit = absCount % 10;
-
-    if (absCount > 10 && absCount < 20) {
-        return `${count} объектов`;
-    }
-    if (lastDigit > 1 && lastDigit < 5) {
-        return `${count} объекта`;
-    }
-    if (lastDigit === 1) {
-        return `${count} объект`;
-    }
-    return `${count} объектов`;
-}
-
-const JournalList = ({ journalList, sort }: JournalListProps) => {
-    return (
-        <div className={styles.container}>
-            {journalList.length > 0 && (
-                <div className={styles.headFilters}>
-                    <div className={styles.count}>
-                        <span style={{ opacity: 0.6 }}>Отображается</span>
-                        <span className={styles.countItem}>
-                            {pluralizeObjects(journalList.length)}
-                        </span>
-                    </div>
-
-                    <div className={styles.count} style={{ marginLeft: "auto" }}>
-                        <span style={{ opacity: 0.6 }}>Сортируется</span>
-                        <span className={styles.countItem}>{sort.label}</span>
-                    </div>
+const JournalList = ({ journalList }: JournalListProps) => {
+    if (journalList.length === 0) {
+        return (
+            <div className={styles.noDocs}>
+                <div className={styles.textNoDocs}>
+                    <IconError />
+                    Не нашли объект <br /> С такими параметрами
                 </div>
-            )}
+            </div>
+        );
+    }
+    return (
+        <>
             {journalList.length > 0 && (
                 <div className={styles.list}>
                     {journalList.map((journal, index) => (
@@ -50,7 +30,7 @@ const JournalList = ({ journalList, sort }: JournalListProps) => {
                     ))}
                 </div>
             )}
-        </div>
+        </>
     );
 };
 
