f318dde759e538e20995e7e7a5a16cf98686a90b
dmtr636 <dmtr636@gmail.com>
2025-10-02 20:11:30 +0300

    Add workbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [svgr(), react()],
+    plugins: [
+        svgr(),
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            strategies: "injectManifest",
+            srcDir: "src",
+            filename: "sw.ts",
+            injectRegister: "auto",
+            devOptions: {
+                enabled: true,
+                type: "module",
+            },
+            manifest: {
+                name: "Build - Kydas",
+                short_name: "Build - Kydas",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#ffffff",
+                icons: [],
+            },
+        }),
+    ],
     define: {
         global: {},
     },
